feb224cd056e9d333ed80493c619ed3742ff6418
Chatur0602 <irule0602@gmail.com>
2023-10-20 11:10:40 +0800

    Add project files.

---
+++ Healthy Haven/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Healthy_Haven.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
