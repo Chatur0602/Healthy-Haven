fb828583a4875b56f7921475537edcddc6f55047
Chatur0602 <irule0602@gmail.com>
2023-10-28 00:03:19 +0800

    Added Delete Functionality

--- Healthy Haven/Controllers/AdminController.cs
+++ Healthy Haven/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using Healthy_Haven.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Healthy_Haven.Controllers
@@ -11,11 +12,13 @@ namespace Healthy_Haven.Controllers
     public class AdminController : Controller
     {
        
-        private readonly UserManager<ApplicationUser> userManager; 
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly ApplicationDbContext _db;
 
-        public AdminController( UserManager<ApplicationUser> userManager)
+        public AdminController( UserManager<ApplicationUser> userManager, ApplicationDbContext db)
         {
             this.userManager = userManager;
+            _db = db;
            
         }
 
@@ -25,6 +28,34 @@ namespace Healthy_Haven.Controllers
          
             return View(users); 
         }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult DeleteUser(String? Id) {
+
+            var userDetails = _db.Users.Find(Id);
+            if(userDetails == null) {
+                return NotFound();
+            }
+
+            return View (userDetails); 
+        
+        }
+
+        [HttpPost]
+        public IActionResult DeleteUserPost(String? Id)
+        {
+            var userDetails = _db.Users.Find(Id);
+            if(userDetails == null)
+            {
+                return NotFound();  
+            }
+
+            _db.Users.Remove(userDetails);  
+            _db.SaveChanges();  
+
+            return RedirectToAction("UserManagement");
+        }
+
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
