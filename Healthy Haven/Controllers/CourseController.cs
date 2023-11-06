2beac0e6230c386c43fa683e788be6103473fc4c
Isaac Tan Kei Yin <tankeiyin0902@gmail.com>
2023-11-06 09:18:57 +0800

    issac

---
+++ Healthy Haven/Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using Healthy_Haven.Data;
+using Healthy_Haven.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Healthy_Haven.Controllers
+{
+	public class CourseController : Controller
+	{
+		private readonly ILogger<CourseController> _logger;
+		private readonly ApplicationDbContext _db;
+
+		public CourseController(ILogger<CourseController> logger, ApplicationDbContext db)
+		{
+			_db = db;
+			_logger = logger;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Healthy Haven/Models/CoursesModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Healthy_Haven.Models
+{
+    public class CoursesModel
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CourseName { get; set; }
+        public string CourseDescription { get; set; }
+
+        public int InstructorId { get; set; }
+    }
+}
