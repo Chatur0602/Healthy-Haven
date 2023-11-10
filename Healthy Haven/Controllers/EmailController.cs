231f44b9db64a8901c0ca9b269241c0d4de2086e
Isaac Tan Kei Yin <tankeiyin0902@gmail.com>
2023-11-10 14:06:14 +0800

    EmailConfirmatin

---
+++ Healthy Haven/Controllers/EmailController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Healthy_Haven.Models;
+
+namespace Identity.Controllers
+{
+    public class EmailController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+        public EmailController(UserManager<ApplicationUser> usrMgr)
+        {
+            userManager = usrMgr;
+        }
+
+        public async Task<IActionResult> ConfirmEmail(string token, string email)
+        {
+            var user = await userManager.FindByEmailAsync(email);
+            if (user == null)
+                return View("Error");
+
+            var result = await userManager.ConfirmEmailAsync(user, token);
+            return View(result.Succeeded ? "ConfirmEmail" : "Error");
+        }
+    }
+}
