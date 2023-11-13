857c458b3f027447453ef25f4e7fac21472c27ba
shiminwee <shi.min.0607@gmail.com>
2023-11-13 16:54:07 +0800

    Able to select dropdown from member and instructor lists

--- Healthy Haven/Controllers/ConsultationsController.cs
+++ Healthy Haven/Controllers/ConsultationsController.cs
@@ -2,6 +2,11 @@
 using System.Diagnostics;
 using Healthy_Haven.Data;
 using Healthy_Haven.Models;
+using Microsoft.AspNetCore.Mvc.Rendering; // For SelectListItem
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+
 
 
 namespace Healthy_Haven.Controllers
@@ -9,21 +14,36 @@ namespace Healthy_Haven.Controllers
     public class ConsultationsController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        // Constructor
-        public ConsultationsController(ApplicationDbContext db)
+        public ConsultationsController(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
         {
             _db = db;
+            _userManager = userManager;
         }
+
         public IActionResult ConsultationsManagement()
         {
-            List<ConsultationsEntity> consultations = new List<ConsultationsEntity>();
-            consultations = _db.Consultations.ToList();
+            List<ConsultationsEntity> consultations = _db.Consultations.ToList();
             return View(consultations);
         }
 
         public IActionResult Create()
         {
+            var members = _userManager.GetUsersInRoleAsync("Member").Result;
+
+            // Get users with the "Instructor" role
+            var instructors = _userManager.GetUsersInRoleAsync("Instructor").Result;
+
+            // Populate the ViewBag.Students and ViewBag.Instructors with lists of SelectListItem
+            ViewBag.Members = members
+                .Select(user => new SelectListItem { Value = user.Id, Text = $"{user.FirstName} {user.LastName}" })
+                .ToList();
+
+            ViewBag.Instructors = instructors
+                .Select(user => new SelectListItem { Value = user.Id, Text = $"{user.FirstName} {user.LastName}" })
+                .ToList();
+
             return View();
         }
 
