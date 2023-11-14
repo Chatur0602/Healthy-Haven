913c93065a58e0cf99db32a7b67b93ce26bd0e08
Chatur0602 <irule0602@gmail.com>
2023-11-14 12:10:21 +0800

    Revert "Merging Into Master Changes"
    This reverts commit 678ece4805101195fe531fcbae39e9bf6eb7c4f9.

---
+++ Healthy Haven/Models/CoursesModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Healthy_Haven.Models
+{
+    public class CoursesModel
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public DateTime course_date { get; set; }
+        public decimal credit_hours { get; set; }
+
+
+        public int instructor_id { get; set; }
+    }
+}
