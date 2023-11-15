14d5f4615fa467fb2721f6691719c1c964767e8f
Chatur0602 <irule0602@gmail.com>
2023-11-15 10:49:56 +0800

    Forums Search & Sort Functionality Built with a bunch of other finer details added

--- Healthy Haven/Controllers/ForumController.cs
+++ Healthy Haven/Controllers/ForumController.cs
@@ -96,7 +96,7 @@ namespace Healthy_Haven.Controllers
                     int fileCount = 1;
 
           
-                 using (var amazonS3client = new AmazonS3Client("ASIA55H4D3RUWDVRPKAU", "yaSA7dZEgLCD+Yl9bbbfFnmAbr7fBDfZpwHjNlaY", "FwoGZXIvYXdzEB0aDNVKLrJDrK/0xOz8rSK8AarOEXpYHK5V3fa0Gu+lLwR54v2ERskVDVQGdmCYwwzoTU7fNuHLdl+ISnj0yOwfq8F3JIRb+H3MUY9z58udIcbaSMc7D0XpMRHTTmLE9BJ21/CrFYKu1KQR2O7gI699OUd4FClUzJymXhBdDCMXW9jNQ5OAsTpnDJ6z6mOHMNdHvjRtO8xSWhgnwHYy0Wwuj+nuDcbEsDNllwcRhn/g40i/FNAYXxkmkXY1h7K+/IPYgv0DrlpXN8gK1nXGKKriy6oGMi3Xp/sg2rzV+FfND+0Vfxj6evcpwsTXrc73LnWtaZn0QWFvC0LJBJ8g4kJKq5g=", RegionEndpoint.USEast1))
+                 using (var amazonS3client = new AmazonS3Client("ASIA55H4D3RU2S4RVAFU", "Fl7B9LWppMYtV6DhtnbTkMkt6r+jWCfnq0Lro+nF", "FwoGZXIvYXdzEDQaDJ1NuXuaLg+h531GiyK8Ab1lNhSn8gWNJJ7HSpEWJsIxqcx3W3B68SiWq+OV5q7lXwk4LuUpW0tY0WsS7cWkKlFLDXFbpPg2bK1xXEwfBl2kPoGKIm1pPsR9QdG2Ha6R2SUi3zfcimj9SY2VpetTxQRQfwuNfxegiffyTXaDRXx5ekbscDxkbkeYBiZW342XnEpKwUw5QjxQpHdoRjWWPtZ7Qp8yfN8dT1ht138xGgKocqYtTh3tcO8pS48koWRxXWuq+whdb+TS/TubKN/f0KoGMi0mjHJfI9PTBqxu4jXFPZ71JMKnSatL4pu+2b9fgh3NaPMYcbyB6bzNCgFPI4Y=", RegionEndpoint.USEast1))
                     {
                         foreach (var file in files)
                         {
@@ -244,7 +244,7 @@ namespace Healthy_Haven.Controllers
                 return NotFound();
             }
 
-            using (var amazonS3client = new AmazonS3Client("ASIA55H4D3RUWDVRPKAU", "yaSA7dZEgLCD+Yl9bbbfFnmAbr7fBDfZpwHjNlaY", "FwoGZXIvYXdzEB0aDNVKLrJDrK/0xOz8rSK8AarOEXpYHK5V3fa0Gu+lLwR54v2ERskVDVQGdmCYwwzoTU7fNuHLdl+ISnj0yOwfq8F3JIRb+H3MUY9z58udIcbaSMc7D0XpMRHTTmLE9BJ21/CrFYKu1KQR2O7gI699OUd4FClUzJymXhBdDCMXW9jNQ5OAsTpnDJ6z6mOHMNdHvjRtO8xSWhgnwHYy0Wwuj+nuDcbEsDNllwcRhn/g40i/FNAYXxkmkXY1h7K+/IPYgv0DrlpXN8gK1nXGKKriy6oGMi3Xp/sg2rzV+FfND+0Vfxj6evcpwsTXrc73LnWtaZn0QWFvC0LJBJ8g4kJKq5g=", RegionEndpoint.USEast1))
+            using (var amazonS3client = new AmazonS3Client("ASIA55H4D3RU2S4RVAFU", "Fl7B9LWppMYtV6DhtnbTkMkt6r+jWCfnq0Lro+nF", "FwoGZXIvYXdzEDQaDJ1NuXuaLg+h531GiyK8Ab1lNhSn8gWNJJ7HSpEWJsIxqcx3W3B68SiWq+OV5q7lXwk4LuUpW0tY0WsS7cWkKlFLDXFbpPg2bK1xXEwfBl2kPoGKIm1pPsR9QdG2Ha6R2SUi3zfcimj9SY2VpetTxQRQfwuNfxegiffyTXaDRXx5ekbscDxkbkeYBiZW342XnEpKwUw5QjxQpHdoRjWWPtZ7Qp8yfN8dT1ht138xGgKocqYtTh3tcO8pS48koWRxXWuq+whdb+TS/TubKN/f0KoGMi0mjHJfI9PTBqxu4jXFPZ71JMKnSatL4pu+2b9fgh3NaPMYcbyB6bzNCgFPI4Y=", RegionEndpoint.USEast1))
             {
                 foreach (var fileName in selectedFileNames)
                 {
