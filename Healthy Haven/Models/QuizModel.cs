9a3e9a93a0b718304d8795421ab0ae0e0b122da7
Rishad Yunus <rishadhussein88@gmail.com>
2023-11-16 00:49:40 +0800

    added quiz model

---
+++ Healthy Haven/Models/QuizModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Healthy_Haven.Models
+{
+    public class QuizModel
+    {
+        [Key]
+        public int quizID { get; set; }
+
+        public string name { get; set; }
+        public string description { get; set; }
+        public string authorID { get; set; }
+        public string date { get; set; }
+        public string quiz_data { get; set; }
+    }
+}
