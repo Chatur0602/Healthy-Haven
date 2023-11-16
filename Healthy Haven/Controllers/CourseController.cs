305b589a6c414c4a10e7db1a5085c9fc05b03853
Isaac Tan Kei Yin <tankeiyin0902@gmail.com>
2023-11-16 13:01:34 +0800

    Halfway done for the image upload

--- Healthy Haven/Controllers/CourseController.cs
+++ Healthy Haven/Controllers/CourseController.cs
@@ -3,6 +3,10 @@ using Healthy_Haven.Models;
 using Healthy_Haven.Views.Course;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
 
 namespace Healthy_Haven.Controllers
 {
@@ -23,10 +27,15 @@ namespace Healthy_Haven.Controllers
             Courses = _db.Courses.ToList();
             return View(Courses);
         }
-        public IActionResult CourseDetails()
+        public IActionResult CourseDetails(int? id)
 		{
-			return View();
-		}
+            var coursedetails = _db.Courses.Find(id);
+            if (coursedetails == null)
+            {
+                return NotFound();
+            }
+            return View("UserCourse", new List<CoursesModel> { coursedetails });
+        }
 		public IActionResult Course()
 		{
 			List<CoursesModel> Courses = new List<CoursesModel>();
@@ -38,13 +47,33 @@ namespace Healthy_Haven.Controllers
         
         public IActionResult CreateCourse()
         {
-            return View();
+            List<CoursesModel> files = new List<CoursesModel>();
+            files = _db.Courses.ToList();
+            return View(files);
         }
 
         [HttpPost]
-		public IActionResult Create(CoursesModel coursedetails)
+		public async IActionResult Create(CoursesModel coursedetails, List<IFormFile> file)
 		{
-			Debug.WriteLine("COURSE NAME= " + coursedetails.name + "COURSE DATE= " + coursedetails.course_date);
+            using (var amazonS3client = new AmazonS3Client("ASIA55H4D3RU5PVRPW4C", "2INDCqTHlj9I0Ps8X2zeZYm/l9l+RTUh3bG825YV", "FwoGZXIvYXdzELz//////////wEaDP93jwo8t0aem23RwyK8AVJ4brFIvJ3Wtp/5Derbf4U4WmkgQujFQlgljVemYiRo1OP7Nh6r0vle8JwrfXcPjFKxqUMphY+nUNQxlyl+WmRJjP9Cf0CLJL+eXvvFdDzzrGp/ykcPn6dD2L2l2vpoIEyB4RvzRkS/PMUj5OFSB/bOxtdupuQyBzIgmycOTyVXcWzjBicH/C+yaeRRWrlDK8rMD4qjKEIPXTVu+w6FyMFL/z3gzyAeou7WtpJ1NiLrE1j6NZ7q5U6rDy+cKKq4tqoGMi39vKCSxdWRH5GgdCEQpi0fEyx+jbX5ofKFyChV2TR5oWyh0MrXP0OhYeYn2oE=", RegionEndpoint.USEast1))
+            {
+                using (var memorystream = new MemoryStream())
+                {
+                    file.CopyTo(memorystream);
+                    var request = new TransferUtilityUploadRequest
+                    {
+                        InputStream = memorystream,
+                        Key = file.FileName,
+                        BucketName = "healthyhavens3",
+                        ContentType = file.ContentType,
+                    };
+
+                    var transferUtility = new TransferUtility(amazonS3client);
+                    await transferUtility.UploadAsync(request);
+                }
+            }
+
+            Debug.WriteLine("COURSE NAME= " + coursedetails.name + "COURSE DATE= " + coursedetails.course_date);
 			_db.Courses.Add(coursedetails);
 			_db.SaveChanges();
 
--- Healthy Haven/Models/CoursesModel.cs
+++ Healthy Haven/Models/CoursesModel.cs
@@ -9,10 +9,10 @@ namespace Healthy_Haven.Models
         public int id { get; set; }
 
         [Required(ErrorMessage = "Please enter the course name.")]
-        public string name { get; set; }
+        public string? name { get; set; }
 
         [Required(ErrorMessage = "Please enter the course description.")]
-        public string description { get; set; }
+        public string? description { get; set; }
 
         [Required(ErrorMessage = "Please enter the course date.")]
         public DateTime course_date { get; set; }
@@ -21,9 +21,10 @@ namespace Healthy_Haven.Models
         public decimal credit_hours { get; set; }
 
         [Required(ErrorMessage = "Please enter the valid instructor id.")]
-        public string instructor_id { get; set; }
+        public string? instructor_id { get; set; }
 
         [ForeignKey("instructor_id")]
         public ApplicationUser Instructor { get; set; }
+        public string? photo {  get; set; }
     }
 }
