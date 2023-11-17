d1e0c09bee5d4c68bf619e741a4f53434048ff03
Isaac Tan Kei Yin <tankeiyin0902@gmail.com>
2023-11-17 14:24:49 +0800

    make changes on the course

--- Healthy Haven/Controllers/CourseController.cs
+++ Healthy Haven/Controllers/CourseController.cs
@@ -7,19 +7,20 @@ using Amazon;
 using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.S3.Transfer;
+using Microsoft.AspNetCore.Identity;
 
 namespace Healthy_Haven.Controllers
 {
-	public class CourseController : Controller
-	{
-		private readonly ILogger<CourseController> _logger;
-		private readonly ApplicationDbContext _db;
+    public class CourseController : Controller
+    {
+        private readonly ILogger<CourseController> _logger;
+        private readonly ApplicationDbContext _db;
 
-		public CourseController(ILogger<CourseController> logger, ApplicationDbContext db)
-		{
-			_db = db;
-			_logger = logger;
-		}
+        public CourseController(ILogger<CourseController> logger, ApplicationDbContext db)
+        {
+            _db = db;
+            _logger = logger;
+        }
 
         public IActionResult UserCourse()
         {
@@ -27,78 +28,96 @@ namespace Healthy_Haven.Controllers
             Courses = _db.Courses.ToList();
             return View(Courses);
         }
+
         public IActionResult CourseDetails(int? id)
-		{
+        {
             var coursedetails = _db.Courses.Find(id);
             if (coursedetails == null)
             {
                 return NotFound();
             }
+
             return View("UserCourse", new List<CoursesModel> { coursedetails });
         }
-		public IActionResult Course()
-		{
-			List<CoursesModel> Courses = new List<CoursesModel>();
-			Courses = _db.Courses.ToList();
 
-			return View(Courses);
-		}
-
-        
-        public IActionResult CreateCourse()
+        public IActionResult Course()
         {
-            List<CoursesModel> files = new List<CoursesModel>();
-            files = _db.Courses.ToList();
-            return View(files);
+            List<CoursesModel> Courses = new List<CoursesModel>();
+            Courses = _db.Courses.ToList();
+            return View(Courses);
         }
 
+        public IActionResult CreateCourse()
+        {
+            var coursedetails = new CoursesModel();
+            return View(coursedetails);
+        }
+
+
+
         [HttpPost]
-		public async Task<IActionResult> Create(CoursesModel coursedetails, List<IFormFile> files)
-		{
-            using (var amazonS3client = new AmazonS3Client("ASIA55H4D3RU5PVRPW4C", "2INDCqTHlj9I0Ps8X2zeZYm/l9l+RTUh3bG825YV", "FwoGZXIvYXdzELz//////////wEaDP93jwo8t0aem23RwyK8AVJ4brFIvJ3Wtp/5Derbf4U4WmkgQujFQlgljVemYiRo1OP7Nh6r0vle8JwrfXcPjFKxqUMphY+nUNQxlyl+WmRJjP9Cf0CLJL+eXvvFdDzzrGp/ykcPn6dD2L2l2vpoIEyB4RvzRkS/PMUj5OFSB/bOxtdupuQyBzIgmycOTyVXcWzjBicH/C+yaeRRWrlDK8rMD4qjKEIPXTVu+w6FyMFL/z3gzyAeou7WtpJ1NiLrE1j6NZ7q5U6rDy+cKKq4tqoGMi39vKCSxdWRH5GgdCEQpi0fEyx+jbX5ofKFyChV2TR5oWyh0MrXP0OhYeYn2oE=", RegionEndpoint.USEast1))
+        public async Task<IActionResult> Create(CoursesModel coursedetails, List<IFormFile> files)
+        {
+            try
             {
+
+                using (var amazonS3client = new AmazonS3Client("ASIA55H4D3RU5PVRPW4C", "2INDCqTHlj9I0Ps8X2zeZYm/l9l+RTUh3bG825YV", "FwoGZXIvYXdzELz//////////wEaDP93jwo8t0aem23RwyK8AVJ4brFIvJ3Wtp/5Derbf4U4WmkgQujFQlgljVemYiRo1OP7Nh6r0vle8JwrfXcPjFKxqUMphY+nUNQxlyl+WmRJjP9Cf0CLJL+eXvvFdDzzrGp/ykcPn6dD2L2l2vpoIEyB4RvzRkS/PMUj5OFSB/bOxtdupuQyBzIgmycOTyVXcWzjBicH/C+yaeRRWrlDK8rMD4qjKEIPXTVu+w6FyMFL/z3gzyAeou7WtpJ1NiLrE1j6NZ7q5U6rDy+cKKq4tqoGMi39vKCSxdWRH5GgdCEQpi0fEyx+jbX5ofKFyChV2TR5oWyh0MrXP0OhYeYn2oE=", RegionEndpoint.USEast1))
+                {
                 foreach (var file in files)
                 {
                     using (var memorystream = new MemoryStream())
                     {
                         file.CopyTo(memorystream);
+                        var key = "CourseImages/" + file.FileName;
                         var request = new TransferUtilityUploadRequest
                         {
                             InputStream = memorystream,
-                            Key = file.FileName,
+                            Key = key,
                             BucketName = "healthyhavens3",
                             ContentType = file.ContentType,
                         };
 
                         var transferUtility = new TransferUtility(amazonS3client);
+                        Debug.WriteLine($"Uploading image to S3. Key: {key}");
                         await transferUtility.UploadAsync(request);
+                        Debug.WriteLine($"Image uploaded successfully.");
+
+                            CoursesModel courseImage = new CoursesModel();
+                        courseImage.photo = file.FileName;
                     }
                 }
             }
-
             Debug.WriteLine("COURSE NAME= " + coursedetails.name + "COURSE DATE= " + coursedetails.course_date);
-			_db.Courses.Add(coursedetails);
-			_db.SaveChanges();
+            _db.Courses.Add(coursedetails);
+            _db.SaveChanges();
 
             return RedirectToAction("Course");
-		}
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (log or provide user feedback)
+                Debug.WriteLine($"An error occurred: {ex.Message}");
+                return RedirectToAction("ErrorPage"); // Redirect to an error page or handle appropriately
+            }
+        }
 
-		public IActionResult Edit(int? id)
-		{
-			var coursedetails = _db.Courses.Find(id);
-			if (coursedetails == null)
-			{
-				return NotFound();
-			}
-			return View("EditCourse", coursedetails);
-		}
 
-		[HttpPost]
+
+        public IActionResult Edit(int? id)
+        {
+            var coursedetails = _db.Courses.Find(id);
+            if (coursedetails == null)
+            {
+                return NotFound();
+            }
+            return View("EditCourse", coursedetails);
+        }
+
+        [HttpPost]
         public IActionResult Edit(CoursesModel coursedetails)
         {
-			
-			_db.Courses.Update(coursedetails);
-			_db.SaveChanges();
+            _db.Courses.Update(coursedetails);
+            _db.SaveChanges();
             return RedirectToAction("Course");
         }
 
@@ -110,7 +129,6 @@ namespace Healthy_Haven.Controllers
             }
 
             var coursedetails = _db.Courses.Find(id);
-
             if (coursedetails == null)
             {
                 return NotFound();
@@ -119,16 +137,15 @@ namespace Healthy_Haven.Controllers
             return View("DeleteCourse", coursedetails);
         }
 
-		[HttpPost]
-		public IActionResult DeleteCourse(int? id)
-		{
+        [HttpPost]
+        public IActionResult DeleteCourse(int? id)
+        {
             if (id == null)
             {
                 return NotFound();
             }
 
             var coursedetails = _db.Courses.Find(id);
-
             if (coursedetails == null)
             {
                 return NotFound();
@@ -139,5 +156,7 @@ namespace Healthy_Haven.Controllers
 
             return RedirectToAction("Course");
         }
-	}
+    }
+
 }
+
